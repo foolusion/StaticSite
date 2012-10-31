94bfe88ef263aaa92afe65eaf1ab3c58d61cbf75
Andrew O'Neill <foolusion@gmail.com>
2012-10-31 18:04:06 -0400

    Added new tests to lex_test.go

--- lex_test.go
+++ lex_test.go
@@ -21,6 +21,8 @@ var lexTests = []lexTest{
 	{"text", "now is the time\nfor all good things", []item{{itemText, "now is the time\nfor all good things"}, tEOF}},
 	{"2par", "go is fun\n\nrodents are gross", []item{{itemText, "go is fun"}, {itemText, "rodents are gross"}, tEOF}},
 	{"h1", "# This is a header\n", []item{{itemH1, "#"}, {itemText, "This is a header"}, tEOF}},
+	{"par head par", "par one\n\n# header\n\npar two",
+		[]item{{itemText, "par one"}, {itemH1, "#"}, {itemText, "header"}, {itemText, "par two"}, tEOF}},
 }
 
 func collect(t *lexTest) (items []item) {
